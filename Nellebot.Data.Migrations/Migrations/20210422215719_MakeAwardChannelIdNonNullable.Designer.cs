d7fbfde586c971a66f01557d70c4bed6a069c1fc
Bob Loblaw <bobloblaw@selfdocumentingcode>
2021-04-22 23:58:00 +0200

    Make AwardChannelId non nullable

--- Nellebot.Data.Migrations/Migrations/20210422215719_MakeAwardChannelIdNonNullable.Designer.cs
+++ Nellebot.Data.Migrations/Migrations/20210422215719_MakeAwardChannelIdNonNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nellebot.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Nellebot.Data.Migrations.Migrations
 {
     [DbContext(typeof(BotDbContext))]
-    partial class BotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210422215719_MakeAwardChannelIdNonNullable")]
+    partial class MakeAwardChannelIdNonNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Nellebot.Data.Migrations/Migrations/20210422215719_MakeAwardChannelIdNonNullable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nellebot.Data.Migrations.Migrations
+{
+    public partial class MakeAwardChannelIdNonNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AwardChannelId",
+                table: "AwardMessages",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AwardChannelId",
+                table: "AwardMessages",
+                type: "numeric(20,0)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)");
+        }
+    }
+}
