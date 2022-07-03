d6286cb6321a9ca84230ffe5edce992f8cebfd1b
Bob Loblaw <razvanmandache@gmail.com>
2022-07-03 09:48:33 +0200

    Migrate to npgsql 6 new timestamp behaviour

--- Nellebot.Data.Migrations/Migrations/20220703074502_MigrateToTimestampTz.Designer.cs
+++ Nellebot.Data.Migrations/Migrations/20220703074502_MigrateToTimestampTz.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nellebot.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Nellebot.Data.Migrations.Migrations
 {
     [DbContext(typeof(BotDbContext))]
-    partial class BotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220703074502_MigrateToTimestampTz")]
+    partial class MigrateToTimestampTz
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
