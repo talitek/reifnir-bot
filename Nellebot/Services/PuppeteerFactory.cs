da7bf2c15cdcd5dca97b793ec3bfa30c7dbb377b
Bob Loblaw <razvanmandache@gmail.com>
2023-02-13 23:29:15 +0100

    Fix name conflict for puppeteer download path

--- Nellebot/Services/PuppeteerFactory.cs
+++ Nellebot/Services/PuppeteerFactory.cs
@@ -1,40 +1,37 @@
-﻿using PuppeteerSharp;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
+using PuppeteerSharp;
 
-namespace Nellebot.Services
+namespace Nellebot.Services;
+
+public class PuppeteerFactory
 {
-    public class PuppeteerFactory
+    public async Task<Browser> BuildBrowser()
     {
-        public async Task<Browser> BuildBrowser()
-        {
-            var downloadPath = Path.Combine(Path.GetTempPath(), "puppeteer");
+        string downloadPath = Path.Combine(Path.GetTempPath(), $"puppeteer-nellebot");
 
-            var browserFetcherOptions = new BrowserFetcherOptions { Path = downloadPath };
-            var browserFetcher = new BrowserFetcher(browserFetcherOptions);
-            await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+        var browserFetcherOptions = new BrowserFetcherOptions { Path = downloadPath };
+        var browserFetcher = new BrowserFetcher(browserFetcherOptions);
+        await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
 
-            var executableDirectory = Directory.EnumerateDirectories(
-                                         Directory.EnumerateDirectories(downloadPath)
-                                        .First())
-                                    .First();
+        var executableDirectory = Directory.EnumerateDirectories(
+                                     Directory.EnumerateDirectories(downloadPath)
+                                    .First())
+                                .First();
 
-            var executableFilename = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                                    ? "chrome.exe"
-                                    : "chrome";
+        var executableFilename = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                                ? "chrome.exe"
+                                : "chrome";
 
-            var executablePath = Path.Combine(executableDirectory, executableFilename);
+        var executablePath = Path.Combine(executableDirectory, executableFilename);
 
-            var options = new LaunchOptions { Headless = true, ExecutablePath = executablePath };
+        var options = new LaunchOptions { Headless = true, ExecutablePath = executablePath };
 
-            var browser = await Puppeteer.LaunchAsync(options);
+        var browser = await Puppeteer.LaunchAsync(options);
 
-            return browser;
-        }
+        return browser;
     }
 }
