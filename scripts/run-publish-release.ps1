f63264455088636a309d79655caf843c839ed34a
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-09-15 21:38:22 +0200

    Make publish release work on workflow_dispatch

--- scripts/run-publish-release.ps1
+++ scripts/run-publish-release.ps1
@@ -1,8 +1,6 @@
 act --directory '..' `
---workflows '.github/workflows/release-v2.yml' `
---input imageTag='latest' `
+--workflows '.github/workflows/publish-release.yml' `
 --env-file '.act.env' `
---eventpath 'act-event.json' `
 --env GITHUB_REF_NAME='refs/heads/main' `
 --secret-file '.secrets' `
 --secret GITHUB_TOKEN="$(gh auth token)" `
