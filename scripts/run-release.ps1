3a51bf7aaee1f78e4d7212ed87554ce1e6917c5b
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-09-15 02:09:59 +0200

    Add perms back
    Remove workflow perms
    
    Add back workflow perm
    
    Add perms to release and remove from workflow
    
    Disable release permissions
    
    What the f
    
    What
    
    OK!
    
    adfsadf fdg
    
    are you kidding me
    
    no fucking way
    
    grasping at straws
    
    please god
    
    what
    
    please
    
    watt
    
    Dumb
    
    This is it

--- scripts/run-release.ps1
+++ scripts/run-release.ps1
@@ -1,7 +1,9 @@
 act --directory '..' `
 --workflows '.github/workflows/release-v2.yml' `
---input imageTag='release-manager' --input createRelease=true `
+--input imageTag='release-manager' `
 --env-file '.act.env' `
+--secret-file '.secrets' `
+--eventpath 'act-event.json' `
 --env GITHUB_REF_NAME='refs/heads/release-manager' `
 --secret GITHUB_TOKEN="$(gh auth token)" `
 $args
